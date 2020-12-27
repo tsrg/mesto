d1445deece2061e9661f86baeff5a2706810de5d
tsrg <tsg@tmedia.fm>
2020-12-27 22:44:52 +0300

    ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,34 @@
 let profileInfo = document.querySelector('.profile__info');
 let formElement = document.querySelector('.popup');
+let profileName = profileInfo.querySelector('.profile__title')
+let popupDescr = formElement.querySelector('.popup__input_type_description');
+let profileDescription = profileInfo.querySelector('.profile__subtitle');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let editButton = document.querySelector('.profile__edit-button');
 
 
-function openPopup() {
-  let profileName = profileInfo.querySelector('.profile__title').innerHTML;
-  let profileDescription = profileInfo.querySelector('.profile__subtitle').innerHTML;
-  let popupName = formElement.querySelector('.popup__input_type_name');
-  let popupDescr = formElement.querySelector('.popup__input_type_description');
-  popupName.value = profileName;
-  popupDescr.value = profileDescription;
-
-  formElement.classList.add('popup_opened');
-}
 
 function closePopup() {
   formElement.classList.remove('popup_opened');
 }
 
+function openPopup() {
+  console.log("www")
+  formElement.classList.add('popup_opened');
+  nameInput.value = profileName.innerHTML;
+  popupDescr.value = profileDescription.innerHTML;
+  formElement.classList.add('popup_opened');
+}
+
 function handleFormSubmit (evt) {
     evt.preventDefault();
-    let nameInput = formElement.querySelector('.popup__input_type_name');
-    let jobInput = formElement.querySelector('.popup__input_type_description');
-    profileInfo.querySelector('.profile__title').textContent = nameInput.value;
+    let jobInput = popupDescr;
+    profileName.textContent = nameInput.value;
     profileInfo.querySelector('.profile__subtitle').textContent = jobInput.value;
     formElement.classList.remove('popup_opened');
 }
 
-
+console.log(document.querySelector('.profile__edit-button'))
 formElement.addEventListener('submit', handleFormSubmit);
+formElement.querySelector('.popup__close-btn').addEventListener('click', closePopup);
+document.querySelector('.profile__edit-button').addEventListener("click", openPopup);
