0d0768d05640353e2bf441def900c68c0725aac9
tsrg <tsg@tmedia.fm>
2020-12-28 23:00:29 +0300

    Поправил недочеты

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ let popupDescr = formElement.querySelector('.popup__input_type_description');
 let profileDescription = profileInfo.querySelector('.profile__subtitle');
 let nameInput = formElement.querySelector('.popup__input_type_name');
 let editButton = document.querySelector('.profile__edit-button');
+let closeButton = formElement.querySelector('.popup__close-btn')
 
 
 
@@ -13,8 +14,7 @@ function closePopup() {
 }
 
 function openPopup() {
-  console.log("www")
-  formElement.classList.add('popup_opened');
+
   nameInput.value = profileName.innerHTML;
   popupDescr.value = profileDescription.innerHTML;
   formElement.classList.add('popup_opened');
@@ -22,13 +22,11 @@ function openPopup() {
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
-    let jobInput = popupDescr;
     profileName.textContent = nameInput.value;
-    profileInfo.querySelector('.profile__subtitle').textContent = jobInput.value;
+    profileDescription.textContent = popupDescr.value;
     closePopup();
 }
 
-console.log(document.querySelector('.profile__edit-button'))
 formElement.addEventListener('submit', handleFormSubmit);
-formElement.querySelector('.popup__close-btn').addEventListener('click', closePopup);
-document.querySelector('.profile__edit-button').addEventListener("click", openPopup);
+closeButton.addEventListener('click', closePopup);
+editButton.addEventListener("click", openPopup);
